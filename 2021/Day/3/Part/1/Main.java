d53f7274c5b6d369bc25ff09d2fc7826cdb50531
Shane M Mathews <shane.m.mathews@gmail.com>
2021-12-03 07:47:22 -0500

    day 3 part 1

---
+++ 2021/Day/3/Part/1/Main.java
@@ -0,0 +1,42 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int numberOfLines = 0;
+        final int numberOfBits = 12;
+        int[] lastIntegersQueue= new int[numberOfBits];
+        try (Scanner scanner = new Scanner(new File("input"))) {
+            while(scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                ++numberOfLines;
+                for (int i = 0; i < numberOfBits; i++) {
+                    lastIntegersQueue[i] += (line.charAt(i) == '1') ? 1 : 0;
+                }
+            }
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        final int halfNumberOfLines = numberOfLines/2;
+        int gamma = 0;//most commmon bits
+        int epsilon = 0;
+        for (int i = 0; i < numberOfBits; i++) {
+            final int toAdd =(1 << (numberOfBits-i-1));
+            if(lastIntegersQueue[i] == halfNumberOfLines) {
+                gamma += toAdd;
+                epsilon += toAdd;
+            }
+            else if(lastIntegersQueue[i] > halfNumberOfLines) {
+                gamma += toAdd;
+            } else {
+                epsilon += toAdd;
+            }
+        }
+
+        System.out.println(gamma*epsilon);
+    }
+}
