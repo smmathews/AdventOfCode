6cd4e5122c429ddba3f376b7de5436e69357cb12
Shane M Mathews <shane.m.mathews@gmail.com>
2022-12-01 10:26:01 -0500

    2022 day 1 successes

---
+++ 2022/01/secondStar.py
@@ -0,0 +1,34 @@
+import heapq
+from dataclasses import dataclass
+
+
+@dataclass
+class FoodItem:
+    calories: int
+
+
+@dataclass
+class Elf:
+    food_items: list[FoodItem]
+
+    def calories_in_food(self) -> int:
+        return sum(f.calories for f in self.food_items)
+
+
+def read_input(filename):
+    elves = []
+    cur_elf = None
+    with open(filename, 'r') as input_file:
+        for i in input_file:
+            if i == '\n':
+                cur_elf = None
+            else:
+                if cur_elf is None:
+                    cur_elf = Elf([])
+                    elves.append(cur_elf)
+                cur_elf.food_items.append(FoodItem(int(i)))
+    print(sum(heapq.nlargest(3, (elf.calories_in_food() for elf in elves))))
+
+
+if __name__ == '__main__':
+    read_input('input')
