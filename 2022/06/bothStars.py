61cff5511db94631ecae2d3278940448cd1ce4e3
Shane M Mathews <shane.m.mathews@gmail.com>
2022-12-06 17:11:33 -0500

    minor refactor

--- 2022/06/bothStars.py
+++ 2022/06/bothStars.py
@@ -3,19 +3,22 @@ from queue import Queue
 
 def read_input(filename, window_length):
     line = open(filename).readline()
-    q = Queue()
+    q = Queue(window_length)
     d = {}
-    for i in range(0, len(line)):
+    for i in range(0, len(line)+1):
+        if q.full():
+            if len(d) == window_length:
+                return i
+            val = q.get()
+            d[val] -= 1
+            if d[val] == 0:
+                d.pop(val)
         char = line[i]
-        if q.qsize() >= window_length:
-            d[q.get()] -= 1
         if char in d:
             d[char] += 1
         else:
             d[char] = 1
         q.put(char)
-        if q.qsize() == window_length and not any(v > 1 for v in d.values()):
-            return i+1
 
 
 if __name__ == '__main__':
