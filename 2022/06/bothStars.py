8fa2710c7368da478a0c8e850f8beb812591ea73
Shane M Mathews <shane.m.mathews@gmail.com>
2022-12-06 16:49:26 -0500

    2022 day 6 star 2 success

--- 2022/06/bothStars.py
+++ 2022/06/bothStars.py
@@ -7,16 +7,17 @@ def read_input(filename, window_length):
     d = {}
     for i in range(0, len(line)):
         char = line[i]
-        if q.qsize() > 3:
+        if q.qsize() >= window_length:
             d[q.get()] -= 1
         if char in d:
             d[char] += 1
         else:
             d[char] = 1
         q.put(char)
-        if q.qsize() == 4 and not any(v > 1 for v in d.values()):
+        if q.qsize() == window_length and not any(v > 1 for v in d.values()):
             return i+1
 
 
 if __name__ == '__main__':
-    print(read_input('input', 4))
+    print("first star:" + str(read_input('input', 4)))
+    print("second star:" + str(read_input('input', 14)))
