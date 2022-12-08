f9ed789076c9ff57cbdc147b67fe0952bdad9a09
Shane M Mathews <shane.m.mathews@gmail.com>
2022-12-08 18:30:10 -0500

    ok, I guess I care. a lot more cleanup.

--- 2022/08/bothStars.py
+++ 2022/08/bothStars.py
@@ -1,27 +1,27 @@
 from dataclasses import dataclass
-
 import numpy as np
-a = np.array([1, 2, 3])
 
 
 @dataclass
 class TreeGrid:
-    trees: np.array = None
+    trees: np.array
 
-    def build(self, in_file):
-        self.trees = np.genfromtxt(in_file, delimiter=1, dtype=np.ubyte)
+    def tree_arrays(self, x, y, from_tree: bool):
+        up = self.trees[0:y, x]
+        down = self.trees[y + 1:self.trees.shape[1], x]
+        left = self.trees[y, 0:x]
+        right = self.trees[y, x + 1:self.trees.shape[0]]
+        if from_tree:
+            return [np.flip(up), down, np.flip(left), right]
+        else:  # from edge
+            return [up, np.flip(down), left, np.flip(right)]
 
     def num_visible_from_outside(self):
         i = 0
         for y in range(0, self.trees.shape[1]):
             for x in range(0, self.trees.shape[0]):
                 elem = self.trees[y, x]
-                up = self.trees[0:y, x]
-                down = np.flip(self.trees[y+1:self.trees.shape[1], x])
-                left = self.trees[y, 0:x]
-                right = np.flip(self.trees[y, x+1:self.trees.shape[0]])
-                to_check_arrays = [up, down, left, right]
-                for to_check in to_check_arrays:
+                for to_check in self.tree_arrays(x, y, False):
                     if len(to_check) == 0 or max(tree for tree in to_check) < elem:
                         i += 1
                         break
@@ -32,27 +32,19 @@ class TreeGrid:
         for y in range(0, self.trees.shape[1]):
             for x in range(0, self.trees.shape[0]):
                 elem = self.trees[y, x]
-                up = np.flip(self.trees[0:y, x])
-                down = self.trees[y+1:self.trees.shape[1], x]
-                left = np.flip(self.trees[y, 0:x])
-                right = self.trees[y, x+1:self.trees.shape[0]]
-                to_check_arrays = [up, down, left, right]
                 scenic_score = 1
-                for to_check in to_check_arrays:
+                for to_check in self.tree_arrays(x, y, True):
                     i = 0
                     for tree in to_check:
                         i += 1
                         if tree >= elem:
                             break
-                    scenic_score = scenic_score*i
+                    scenic_score = scenic_score * i
                 best = max(best, scenic_score)
         return best
 
 
 if __name__ == '__main__':
-    grid = TreeGrid()
-    with open('input') as file:
-        grid.build(file)
-
+    grid = TreeGrid(np.genfromtxt('input', delimiter=1, dtype=np.ubyte))
     print("first star:" + str(grid.num_visible_from_outside()))
     print("second star:" + str(grid.highest_scenic_score_possible()))
