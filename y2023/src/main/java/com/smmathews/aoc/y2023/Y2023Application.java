ca711737dde6bd7a681814a7623d1691d1cf43d9
Shane M Mathews <shane.m.mathews@gmail.com>
2023-12-01 06:42:19 -0500

    init 2023

---
+++ y2023/src/main/java/com/smmathews/aoc/y2023/Y2023Application.java
@@ -0,0 +1,13 @@
+package com.smmathews.aoc.y2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Y2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Y2023Application.class, args);
+	}
+
+}
---
+++ y2023/src/test/java/com/smmathews/aoc/y2023/Y2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smmathews.aoc.y2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Y2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
