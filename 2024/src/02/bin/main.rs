185393279929e9fc1dd6de8a5827988a8fd9db62
Shane M Mathews <shane.m.mathews@gmail.com>
2024-12-03 20:18:13 -0500

    y2024, day 02, star 2

--- 2024/src/02/bin/main.rs
+++ 2024/src/02/bin/main.rs
@@ -34,8 +34,22 @@ fn main() {
     let my_str = include_str!("input");
     let reports = read_lines(&my_str);
     let mut num_safe : u64 = 0;
+    let mut num_safe_with_remove : u64 = 0;
     for report in reports {
-        num_safe += if report.is_safe() {1} else {0};
+        if report.is_safe() {
+            num_safe += 1;
+            num_safe_with_remove += 1;
+        } else {
+            for i in 0..report.levels.len() {
+                let mut with_remove = report.levels.clone();
+                with_remove.remove(i);
+                if(Report{levels: with_remove }.is_safe()) {
+                    num_safe_with_remove += 1;
+                    break;
+                }
+            }
+        };
     }
     println!("star 1 {}", num_safe);
+    println!("star 2 {}", num_safe_with_remove);
 }
